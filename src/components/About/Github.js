76264099443d47c5a5a6120e963dc96b0eb91281
Nikhil Raikwar <126591020+geek-nikhil@users.noreply.github.com>
2025-02-06 18:57:58 +0530

    Update Github.js

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,5 +1,4 @@
 import React from "react";
-import LeetCodeCalendar from 'leetcode-calendar';
 import { Row } from "react-bootstrap";
 
 function Github() {
@@ -21,17 +20,11 @@ function Github() {
   }
   return (
     (
+      <>
       <div>
-        <LeetCodeCalendar
-          username='nikhil846' // Replace with your LeetCode username
-          blockSize={15} // Optional: Size of each block in pixels (default: 15)
-          blockMargin={5} // Optional: Margin between blocks in pixels (default: 5)
-          fontSize={16} // Optional: Font size of the text within blocks (default: 16)
-          theme={exampleTheme} // Optional: A custom theme object to style the calendar
-          style={{ maxWidth: '1100px', margin:'120px'}} // Optional: Inline styles for the calendar container
-        />
       </div>
-    )
+    </>  
+  )
   );
 }
 
